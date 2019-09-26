333e56c7892ff3dccc664a6017005729effe2bf4
Lara Virgínia Gusmão Franca Chaves <51674659+Laravgfc@users.noreply.github.com>
2019-09-26 20:32:42 -0300

    Update planta_de_nivel.ino

--- planta_de_nivel.ino
+++ planta_de_nivel.ino
@@ -1,5 +1,4 @@
 #include <Ultrasonic.h>
-
 const int echoPin = 7; //PINO DIGITAL UTILIZADO PELO HC-SR04 ECHO(RECEBE)
 const int trigPin = 8; //PINO DIGITAL UTILIZADO PELO HC-SR04 TRIG(ENVIA)
  
@@ -10,32 +9,35 @@ String result; //VARIÁVEL DO TIPO STRING
 float dist;
 int i;
 int conver;
-
 void setup(){
   Serial.begin(9600);
-
 }
-
 void loop(){
   dist = 0;
   for(i=0;i<=1000;i++){
   hcsr04(); // FAZ A CHAMADA DO MÉTODO "hcsr04()"
   dist=dist+distancia;
-  delay(1);
+  delayMicroseconds(100);
   }
   dist=dist/1000;
   conver = (19.22*dist)-75.72;
-  
+  if(conver>255) {
+    conver=255;
+  }
+  else if (conver<0) {
+    conver = 0;
+  }
+  else {
+    conver = conver;
+  }
+  analogWrite(3, conver);
   Serial.print("Distancia= "); //IMPRIME O TEXTO NO MONITOR SERIAL
   Serial.print(dist); ////IMPRIME NO MONITOR SERIAL A DISTÂNCIA MEDIDA
   Serial.print("cm  "); //IMPRIME O TEXTO NO MONITOR SERIAL
   Serial.print("PWM= ");
-  Serial.print(conver);
-  Serial.println("V ");
-
+  Serial.println(conver);
+ 
 }
-
-
 //MÉTODO RESPONSÁVEL POR CALCULAR A DISTÂNCIA
 void hcsr04(){
     digitalWrite(trigPin, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
@@ -48,5 +50,5 @@ void hcsr04(){
     //NA VARIAVEL "distancia"
     distancia = (ultrasonic.Ranging(CM)); //VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
     result = String(distancia); //VARIÁVEL GLOBAL DO TIPO STRING RECEBE A DISTÂNCIA(CONVERTIDO DE INTEIRO PARA STRING)
-    delay(1); //INTERVALO DE 500 MILISSEGUNDOS
+    delayMicroseconds(100); //INTERVALO DE 500 MILISSEGUNDOS
  }
